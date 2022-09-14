376ef4bd35ffe45996b43b2f23a4a0f721ed701a
unknown <Tekadon007@gmail.com>
2022-09-14 18:19:25 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+const users = [
+  {
+    name: "Ramin",
+    age: 18,
+    password: "crytek",
+    isLogin: true,
+  },
+  {
+    name: "Islam",
+    age: 21,
+    password: "lagaga",
+    isLogin: true,
+  },
+  {
+    name: "Daniel",
+    age: 19,
+    password: "lohhh",
+    isLogin: true,
+  },
+];
+
+let newUser = {
+  name: null,
+  age: null,
+  password: null,
+  isLogin: false,
+};
+
+const userName = prompt("Имя");
+const userAge = prompt("Возраст");
+const userPassword = prompt("Пароль");
+let checkPassword = prompt("Повторить пароль");
+// if (checkPassword != userPassword) {
+//   let checkPassword = prompt("Подтвеждение пароля");
+// }
+
+
+newUser.name = userName;
+newUser.age = userAge;
+newUser.password = userPassword;
+
+function registerUser() {
+  let { age, name, password } = newUser;
+  if (age !== null && name !== null && password !== null) {
+    const checkUniqueUsername = users.find(item => item.name === newUser.name);
+    if (checkUniqueUsername) return alert("Такое имя пользователя уже есть");
+    users.push(newUser);
+  }
+}
+
+function checkRegister() {
+  const checkLogin = users.find(item => item.name === newUser.name);
+  if (checkLogin) newUser.isLogin = true;
+}
+
+registerUser();
+checkRegister();
+console.log(newUser);
+console.log(users);
